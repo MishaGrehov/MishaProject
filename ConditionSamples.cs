fd74e12c41463f821e1e13a323c0ca7b37a9fb36
MishaGrehov <112182238+MishaGrehov@users.noreply.github.com>
2022-08-28 09:10:48 +0300

    Add files via upload

---
+++ ConditionSamples.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class ConditionSamples
+    {
+        static bool CombineByAnd(bool a, bool b, bool c)
+        {
+            bool result = a || b || c;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static bool CombineByAnd(bool a, bool b, bool c)
+        {
+            bool result = a && b && c;
+            return result;
+        }
+
+        static bool CombineByOr(bool a, bool b, bool c)
+        {
+            bool result = a || b || c;
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            bool a = false;           
+            bool b = true;
+            bool c = true;
+            bool d = CombineByAnd(a, b, c);
+            bool f = CombineByOr(a, b, c);
+            Console.WriteLine(d);
+            Console.WriteLine(f);
+        }
+    }
+}
