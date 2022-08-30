42ea6a7b2fb2e1a3d9abe36d7073c89347bc65aa
MishaGrehov <112182238+MishaGrehov@users.noreply.github.com>
2022-08-30 08:16:37 +0300

    Add files via upload

---
+++ user questionnaire application/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace user_questionnaire_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter your name:");
+            string name = Console.ReadLine();
+            Console.Write("Please, enter your surname:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Nice to meet you " + name  +" " +   surname + "!");
+            Console.Write("Let me ask one more question, how old are you?");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("So this is information about you:");
+            Console.WriteLine(name + " " + surname + "," +" " + age + " " + "years old");
+        }
+    }
+}
---
+++ value types/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace value_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type              Size of Type (in bytes)                  Min                       Max");
+            Console.WriteLine("----------------------------------------------------------------------------------------");
+           
+            Console.WriteLine("System.Int32            4                              -2147483647            2147483648");
+                     
+            Console.WriteLine("System.Byte             1                                    1                       102");
+        }
+    }
+}
